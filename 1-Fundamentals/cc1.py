d56ce45dcb3f6decfa14ec3bad1a3164aec9d4f1
Terrel Brinkley <terrel@terrelbrinkley.com>
2022-08-07 17:09:54 -0400

    NuCamp Python Projects

---
+++ 1-Fundamentals/cc1.py
@@ -0,0 +1,17 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", 'Bellevue']
+
+a = 1
+print(type(a))
+
+b = 100.0
+print(type(b))
+
+c = 'Nyla'
+print(type(c))
+
+d = a + b
+print(d)
+x = 1+(0*10)*3/8**1
+print(x)
