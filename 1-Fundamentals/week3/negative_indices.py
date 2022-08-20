266136a913b46f06354bc8cf37b1291c557d6a94
Terrel Brinkley <terrel@terrelbrinkley.com>
2022-08-20 03:45:14 -0400

    NuCamp Python Projects

---
+++ 1-Fundamentals/week3/negative_indices.py
@@ -0,0 +1,11 @@
+cars = ["lexus", "bmw", "toyota", "nissan"]
+
+# printing a specific index location
+print(cars[2])
+print(cars[-2])
+
+# printing a specific index location with slice
+print(cars[2:4])
+
+# printing a specific index location using negative indices
+print(cars[-4:-2])
